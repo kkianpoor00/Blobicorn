e19e5c496255b8fba6ff03db0b897041aa42731b
zkim-chi <52217069+zkim-chi@users.noreply.github.com>
2019-07-16 12:41:33 -0700

    Add files via upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,3 +2,11 @@ class UsersController < ApplicationController
   def new
   end
 end
+
+# class UsersController < CLearance::UsersController
+#   private
+#
+#   def user_params
+#     params.require(:user).permit(:username, :email, :password)
+#   end
+# end
