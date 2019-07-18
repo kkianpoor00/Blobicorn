dad1e8e7353c859b68be3567ee6235c1f09e03a9
James <jn.northway@gmail.com>
2019-07-18 13:53:28 -0700

    Added search feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if(params[:term])
+      @posts = Post.where('title LIKE ?', "%#{params[:term]}%")
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
--- app/controllers/posts_pages_controller.rb
+++ app/controllers/posts_pages_controller.rb
@@ -4,7 +4,11 @@ class PostsPagesController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if(params[:term])
+      @posts = Post.where('title LIKE ?', "%#{params[:term]}%")
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
